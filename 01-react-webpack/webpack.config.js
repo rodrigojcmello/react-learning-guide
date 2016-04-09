5aa96adcc5f207e464c4c829a1f53d3a314a9e43
Rodrigo Mello <rodrigojcmello@gmail.com>
2016-04-09 00:06:52 -0300

    primeiro exemplo básico

---
+++ 01-react-webpack/webpack.config.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './index.jsx',
+    output: { path: __dirname, filename: 'bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+};
