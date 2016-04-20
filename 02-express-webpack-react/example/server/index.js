a0357845ca7f8cc5088834476d285b34987f26ed
rodrigojcmello <rodrigojcmello@gmail.com>
2016-04-20 09:44:14 -0300

    Update index.js

--- 02-express-webpack-react/example/server/index.js
+++ 02-express-webpack-react/example/server/index.js
@@ -1,10 +1,16 @@
+// Express
+
 import express from 'express';
 const app = express();
+
+// Webpack
 
 import webpack from 'webpack';
 import webpackConfig from '../webpack.config.js';
 import webpackMiddleware from 'webpack-dev-middleware';
 const compiler = webpack(webpackConfig);
+
+// Configuração do Express
 
 app.use(webpackMiddleware(compiler));
 app.get('*');
